8ec30da9596b855b5ca9aa89777f77874b5ce7ad
Oussama Yousfi <yousfioussamaa@gmail.com>
2023-01-27 04:08:27 +0100

    EthSwap smart contract start coding

--- src/contracts/EthSwa.sol
+++ src/contracts/EthSwa.sol
@@ -4,5 +4,9 @@ import "./Token.sol";
 
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
-    
+    Token public token;
+    constructor(Token _token) public {
+        token - _token;
+    }
+
 }
