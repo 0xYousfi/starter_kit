b452722160877907f890df791dd3ca27da8365a8
Oussama Yousfi <yousfioussamaa@gmail.com>
2023-01-27 04:06:41 +0100

    EthSwap test and starting EthSwap smart contract codification

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
-const Migrations = artifacts.require("EthSwap");
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
+
+module.exports = async function(deployer) {
+    // Deploy Token
+    await deployer.deploy(Token)
+    const token = await Token.deployed()
+  
+    // Deploy EthSwap
+    await deployer.deploy(EthSwap)
+    const ethSwap = await EthSwap.deployed()
+  
+    // Transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+  };
